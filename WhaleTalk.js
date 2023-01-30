ef0024e136d18dc6f9978ab754ba44466ac7800e
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-30 21:12:08 +0100

    WhaleTalk_exerciseDone

--- WhaleTalk.js
+++ WhaleTalk.js
@@ -1,11 +1,21 @@
 // WhaleTalk
-const input = "All You need is Code and a hint of cafeine";
+const input = "all You need is Code and a little hint of cafeine";
 const vowels = ["a", "e", "i", "o", "u"];
 let resultArray = [];
 
 for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
-  console.log("input index i : ${inputIndex}");
-  console.log(inputIndex);
+  if (input[inputIndex] === "e") {
+    resultArray.push(input[inputIndex]);
+  }
+  if (input[inputIndex] === "u") {
+    resultArray.push(input[inputIndex]);
+  }
+  for (let vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++) {
+    if (input[inputIndex] === vowels[vowelIndex]) {
+      resultArray.push(input[inputIndex]);
+    }
+  }
 }
-
-console.log(input);
+console.log(resultArray);
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString)
