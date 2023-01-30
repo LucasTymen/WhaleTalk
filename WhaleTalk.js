64767432ad79094e9536890d37e030b9e6bf98c3
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-30 11:49:40 +0100

    WhaleTalk

--- WhaleTalk.js
+++ WhaleTalk.js
@@ -1,1 +1,11 @@
 // WhaleTalk
+const input = "All You need is Code and a hint of cafeine";
+const vowels = ["a", "e", "i", "o", "u"];
+let resultArray = [];
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  console.log("input index i : ${inputIndex}");
+  console.log(inputIndex);
+}
+
+console.log(input);
